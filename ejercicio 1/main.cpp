22972a07ab44f57c335bcd6593e4c99f4016c539
Jose Luis Romero Vazquez <105326766+Jose-Luis-Romero-Vazquez@users.noreply.github.com>
2022-05-11 22:23:02 -0500

    Add files via upload

---
+++ ejercicio 1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+//José Luis Romero Vázquez
+//311869
+//21/01/19
+int main()
+{
+    cout << "*********************************" ;
+    cout << "\n/////////////////////////////////";
+    cout << "\n*\t\t\t\t*";
+    cout << "\n*\t\t\t\t*";
+    cout << "\n*\tBienvenido a C++!!\t*";
+    cout << "\n*\t\t\t\t*";
+    cout << "\n*\t\t\t\t*";
+    cout << "\n/////////////////////////////////";
+     cout << "\n*********************************"<<endl;
+
+
+    return 0;
+}
